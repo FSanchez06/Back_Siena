7951957d2aa32bba81a1c4a434b6145dde90b791
FSanchez06 <felipesanchezcon2@gmail.com>
2024-12-04 11:52:14 -0500

    se arreglo los links

--- server.js
+++ server.js
@@ -46,18 +46,11 @@ cloudinary.config({
 app.use(myconn(mysql, dbOptions, 'single')); // Conexión a la base de datos
 app.use(express.json()); // Permite recibir datos en formato JSON
 app.use(fileUpload({ useTempFiles: true, tempFileDir: path.join(__dirname, "tmp/") }));
-app.use(
-    cors({
-      origin: "https://front-siena.vercel.app", // Dominio de tu frontend en Vercel
-      methods: "GET,POST,PUT,DELETE",
-      credentials: true, // Si necesitas enviar cookies o encabezados de autenticación
-    })
-  );
 
   const allowedOrigins = [
     "https://front-siena.vercel.app",
-    "https://front-siena-git-main-felipesanchezcon2-gmailcoms-projects.vercel.app",
-    "https://front-siena-73fbwl5dh-felipesanchezcon2-gmailcoms-projects.vercel.app"
+    "https://front-siena-felipesanchezcon2-gmailcoms-projects.vercel.app",
+    "https://front-siena-git-main-felipesanchezcon2-gmailcoms-projects.vercel.app"
   ];
   
   app.use(
