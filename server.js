bb4c12735c63e3f43e1a13c67e20b4ede13d46f3
FSanchez06 <felipesanchezcon2@gmail.com>
2024-12-03 20:55:50 -0500

    si

--- server.js
+++ server.js
@@ -47,11 +47,31 @@ app.use(express.json()); // Permite recibir datos en formato JSON
 app.use(fileUpload({ useTempFiles: true, tempFileDir: path.join(__dirname, "tmp/") }));
 app.use(
     cors({
-      origin: "https://front-siena-2s4qpu4sn-felipesanchezcon2-gmailcoms-projects.vercel.app/", // Dominio de tu frontend en Vercel
+      origin: "https://front-siena.vercel.app", // Dominio de tu frontend en Vercel
       methods: "GET,POST,PUT,DELETE",
       credentials: true, // Si necesitas enviar cookies o encabezados de autenticación
     })
   );
+
+  const allowedOrigins = [
+    "https://front-siena.vercel.app",
+    "https://front-siena-2s4qpu4sn-felipesanchezcon2-gmailcoms-projects.vercel.app"
+  ];
+  
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true, // Si envías cookies o encabezados de autenticación
+    })
+  );
+  
 
 // Ruta principal para verificación de conexión
 app.get("/api", (req, res) => {
